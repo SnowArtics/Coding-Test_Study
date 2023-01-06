96a89eaf3c8eb075c647901e6c6a8b9ec6434939
SnowArtics <snowartics@gmail.com>
2023-01-06 20:13:13 +0900

    FEAT : Easy Algorithm
    Upload greedy algorithm : until 1
    Upload implementation : Time, Up Down Left Right

---
+++ implementation/Easy/Time.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool threeTest(int i, int j, int k) {
+    if (i / 10 == 3 || i % 10 == 3 || j / 10 == 3 || j % 10 == 3 || k / 10 == 3 || k % 10 == 3) {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    int n;
+    int output = 0;
+
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < 60; j++) {
+            for (int k = 0; k < 60; k++) {
+                if (threeTest(i, j, k) == true) {
+                    output++;
+                }
+            }
+        }
+    }
+
+    cout << output;
+}
