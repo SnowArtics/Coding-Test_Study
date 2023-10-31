44d1cb24b7cd9b2b60cee623b340c760b90583fa
Snow <84330600+SnowArtics@users.noreply.github.com>
2023-11-01 00:11:41 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string s;
+    int result[26];
+    
+    for(int i=0;i<26;i++){
+        result[i]=0;
+    }
+    
+    cin>>s;
+    
+    for(auto c : s){
+        result[c-'a']++;
+    }
+    
+    for(int i=0;i<26;i++){
+        cout<<result[i]<<' ';
+    }
+}
