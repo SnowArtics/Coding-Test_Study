0c0df10dc32ca43ff18503b8d18b9c8e5fbe240e
Snow <84330600+SnowArtics@users.noreply.github.com>
2024-05-13 23:25:10 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -15,10 +15,10 @@ int main() {
 	arr[1] = 0;
 	for (int i = 2; i < X + 1; ++i) {
 		arr[i] = arr[i - 1] + 1;
-		if (i % 3 == 0)
-			arr[i] = min(arr[i],arr[i / 3] + 1);
 		if (i % 2 == 0)
 			arr[i] = min(arr[i],arr[i / 2] + 1);
+		if (i % 3 == 0)
+			arr[i] = min(arr[i],arr[i / 3] + 1);
 	}
 
 	cout << arr[X];
