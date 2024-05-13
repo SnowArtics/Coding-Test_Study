3d56ce0226e7783ebb26cc4c21aa5aa570fe7203
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2024-05-13 23:26:11 +0900

    featt : solve DP Problem
    Solve 1로 만들기
    this is DP Problem
    BAEKJOON(1463)

---
+++ DynamicPrograming/1로 만들기.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1000001];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int X;
+
+	cin >> X;
+
+	arr[1] = 0;
+	for (int i = 2; i < X + 1; ++i) {
+		arr[i] = arr[i - 1] + 1;
+		if (i % 2 == 0)
+			arr[i] = min(arr[i],arr[i / 2] + 1);
+		if (i % 3 == 0)
+			arr[i] = min(arr[i],arr[i / 3] + 1);
+	}
+
+	cout << arr[X];
+
+	return 0;
+}
