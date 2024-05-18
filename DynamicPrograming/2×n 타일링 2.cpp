7f31bd3313ec45f4ab1b74d76ac88877590366a2
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2024-05-18 21:34:40 +0900

    feat : solve DP Problem
    Solve 2×n 2 타일링
    this is DP Problem
    BAEKJOON(11727)

---
+++ DynamicPrograming/2×n 타일링 2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1001];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+
+	cin >> n;
+
+	arr[1] = 1;
+	arr[2] = 3;
+
+	for (int i = 3; i < n + 1; ++i) {
+		arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007;
+	}
+
+	cout << arr[n];
+
+	return 0;
+}
