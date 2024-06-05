dbbff3eded399667caf7b03e5509f0981a71baca
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2024-06-05 20:32:17 +0900

    Update 수 찾기.cpp

--- BinarySearch/수 찾기.cpp
+++ BinarySearch/수 찾기.cpp
@@ -2,39 +2,44 @@
 
 using namespace std;
 
-int arr01[100002];
+int arr[100001];
+int checkArr[100001];
 
-int n;
+int BinarySearch(int _num, int _size) {
+	int st = 0;
+	int en = _size;
 
-bool FindNum(int _a){
-    int _small = 0;
-    int _large = n-1;
-    
-    while(_small<=_large){
-        int pivot = (_small + _large)/2;
-        if(arr01[pivot]<_a)
-            _small = pivot+1;
-        else if(arr01[pivot]>_a)
-            _large = pivot-1;
-        else
-            return 1;
-    }    
-    return 0;
+	while (st <= en) {
+		int mid = (st + en) / 2;
+		if (arr[mid] < _num)
+			st = mid + 1;
+		else if (arr[mid] < _num)
+			en = mid - 1;
+		else
+			return 1;
+	}
+
+	return 0;
 }
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin>>n;
-    
-    for(int i=0;i<n;++i) cin >> arr01[i];    
-    sort(arr01, arr01+n);
-    int m;
-    cin >> m;
-    for(int i=1;i<m+1;++i){
-        int t;
-        cin>>t;
-        cout<<FindNum(t)<<'\n';
-    }
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; ++i)
+		cin >> checkArr[i];
+
+	for (int i = 0; i < m; ++i)
+		cout << BinarySearch(checkArr[i], n);
+
+	return 0;
 }
